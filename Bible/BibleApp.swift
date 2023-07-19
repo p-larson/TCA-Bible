737aaa7c4746e28a669764aa8ad8535a122c84c2
Peter Larson <p.larson@utexas.edu>
2023-07-19 09:51:11 -0500

    Initial Commit

---
+++ Bible/BibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleApp.swift
+//  Bible
+//
+//  Created by Peter Larson on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
